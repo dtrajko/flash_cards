43985b5ee67b0448ffa851c67226c7b5c86971fc
Dejan Trajkovic <dtrajko@gmail.com>
2017-10-21 01:53:24 -0400

    More terms and vocabulary items

---
+++ database/dumps/flash_cards.settings.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS `settings`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `settings` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `value` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+INSERT INTO `settings` VALUES (1,'score','0','2017-10-21 09:20:26','2017-10-21 09:50:55'),(2,'score_total','21','2017-10-21 09:20:26','2017-10-21 09:41:32');
